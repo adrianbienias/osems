664ff3a8e0acaf302c9754d780125e506a89f902
Adrian Bienias <adrianbienias@gmail.com>
2023-07-10 23:21:55 +0200

    feat: Update homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import MetaHead from "@/components/meta-head"
 import { Navbar } from "@/components/navbar"
-import { readFileSync } from "fs"
-import { marked } from "marked"
 
-export default function Home({ readmeHtml }: { readmeHtml: string }) {
+export default function Home() {
   return (
     <>
       <MetaHead title="Readme" />
@@ -11,20 +9,15 @@ export default function Home({ readmeHtml }: { readmeHtml: string }) {
       <Navbar />
 
       <main>
-        <div
-          dangerouslySetInnerHTML={{ __html: readmeHtml }}
-          className="mb-16"
-        />
+        <h1>OSEMS</h1>
+
+        <p>Open Source Email Marketing Software</p>
+        <p>
+          <a href="https://osems.dev" target="_blank" rel="noopener">
+            🗒️ Check documentation »
+          </a>
+        </p>
       </main>
     </>
   )
-}
-
-export async function getServerSideProps() {
-  const readmeFile = readFileSync("./README.md", { encoding: "utf-8" })
-  const readmeHtml = marked.parse(readmeFile)
-
-  return {
-    props: { readmeHtml },
-  }
 }
