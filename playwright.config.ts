f6a297bb9af9efee924b3a4c51307c16be973a24
Adrian Bienias <adrianbienias@gmail.com>
2023-07-22 11:06:45 +0200

    build: Separate prisma schemas explicitly by database type

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.APP_URL,
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -65,6 +65,6 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run dev:e2e",
-    url: process.env.APP_URL,
+    url: process.env.NEXT_PUBLIC_BASE_URL,
   },
 })
